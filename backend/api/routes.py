5ef7e85e9950132a0fc2ea7c27edafd7dbe2d530
priyathamtarak <priyathamtarak2002@gmail.com>
2025-12-09 09:23:15 +0530

    Added clients Module

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 from api.v1.auth import auth_router
 from api.v1.user import user_router
+from api.v1.clients.client import client_router
+from api.v1.clients.contract import contract_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
+api_router.include_router(client_router)
+api_router.include_router(contract_router)
